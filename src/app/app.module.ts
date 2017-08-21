66e852be8bbbddb2b3364694d8a5610ebf8d7ce7
corawym <corawym@gmail.com>
2017-08-21 15:12:42 -0700

    set up all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EncountersComponent } from './components/encounters/encounters.component';
+import { ReportComponent } from './components/report/report.component';
+import { RegisterComponent } from './components/register/register.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncountersComponent,
+    ReportComponent,
+    RegisterComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
