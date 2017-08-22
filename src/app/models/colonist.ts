0bb33f26e7c3f484a5ee1e1046218d5f51de1f4c
corawym <corawym@gmail.com>
2017-08-22 11:06:40 -0700

    added services

--- src/app/models/colonist.ts
+++ src/app/models/colonist.ts
@@ -6,3 +6,10 @@ export interface Colonist {
   job: Job;
   age: number;
 }
+
+export interface NewColonist {
+  name: string;
+  age: string;
+  job_id: string;
+}
+
--- src/app/models/report.ts
+++ src/app/models/report.ts
@@ -4,5 +4,11 @@ export interface Report {
   action: string;
   colonist_id: number;
   atype: string;
-  
+}
+
+export interface NewReport {
+  atype: string;
+  date: string;
+  action: string;
+  colonist_id: string;
 }
